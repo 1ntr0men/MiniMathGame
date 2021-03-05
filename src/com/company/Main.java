56801b03c0a03cb10d7786af6bf5cc8251ab1dcf
1ntr0men <vadruf07@gmail.com>
2021-03-05 18:51:06 +0300

    Add multiply, minus and minus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,55 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+    public static int generateRandomInt(int i) { // Генератор рандомного числа
+        Random random = new Random();
+        return random.nextInt(i);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("=========Mathematic Quest=========");
-        Quest quest = new Quest();
-        for (int i = 0; i < 3; i++) {
-            Primer zadanie = quest.getPrimer();
-            System.out.println(zadanie.getQuestion());
+        int answer1;
+        int score = 0;
+        System.out.println("=========Mathematics Quest=========");
+        for (int i = 0; i < 10; i++) {
+            int a = generateRandomInt(10);
+            int b = generateRandomInt(10);
+            int chooseSymbol = generateRandomInt(4);
+            switch (chooseSymbol) {
+                case 0 -> {
+                    Plus primer = new Plus(a, b);
+                    answer1 = primer.getAnswer();
+                    System.out.println(primer.getQuestion());
+                }
+                case 1 -> {
+                    Minus primer = new Minus(a, b);
+                    answer1 = primer.getAnswer();
+                    System.out.println(primer.getQuestion());
+                }
+                case 2 -> {
+                    Multiply primer = new Multiply(a, b);
+                    answer1 = primer.getAnswer();
+                    System.out.println(primer.getQuestion());
+                }
+                default -> {
+                    Del primer = new Del(a, b);
+                    answer1 = primer.getAnswer();
+                    System.out.println(primer.getQuestion());
+                }
+            }
             int answer = in.nextInt();
-            if (answer == zadanie.getAnswer()) {
+            if (answer == answer1) {
                 System.out.println("ВЕРНО");
+                score += 10;
             } else {
                 System.out.println("ОШИБКА");
             }
             System.out.println("-----");
-            quest.scorePlus();
         }
-        System.out.println("Ваши очки: " + quest.getScore());
+        System.out.println("Ваши очки: " + score);
     }
 }
--- src/com/company/Primer.java
+++ src/com/company/Primer.java
@@ -1,23 +1,10 @@
 package com.company;
 
 public class Primer {
-    public String question;
-    public int answer;
+    public int a,b;
 
-    public Primer(String question, int answer) {
-        this.question = question;
-        this.answer = answer;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public int getAnswer() {
-        return answer;
-    }
-
-    public boolean isRight(String answer) {
-        return answer.equals(this.answer);
+    public Primer(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
 }
